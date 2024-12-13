6898c21324f6f8bb5de170e1cda8d1612108427d
Warren Mann <warren@nonvol.io>
2024-12-13 10:18:38 -0600

    Added logging and comments

--- http.h
+++ http.h
@@ -1,14 +1,14 @@
 /**
- * @file http.h
+ * @file HTTP.h
  * @author Warren Mann (warren@nonvol.io)
- * @brief nvhttp http module declarations.
+ * @brief nvHTTP HTTP module declarations.
  * @version 0.1.0
  * @date 2024-11-28
  * @copyright Copyright (c) 2024
  */
 
-#ifndef HTTP_H
-#define HTTP_H
+#ifndef http_H
+#define http_H
 
 #include <netinet/in.h>
 #include <stdlib.h>
@@ -16,18 +16,31 @@
 
 #include "log.h"
 
+/**
+ * @brief Represents a variable, either a query parameter, which is passed as 
+ * part of the URI, or a header.
+ */
 typedef struct http_variable_s {
     char *var;
     char *val;
     struct http_variable_s *next;
 } http_variable_s;
 
+/**
+ * @brief Represents the HTTP server. The socket is tracked for accepting 
+ * connections, the log handle for logging output and the address structure.
+ */
 typedef struct http_server_s {
     log_s *log;
     int fd;
     struct sockaddr_in addr;
 } http_server_s;
 
+/**
+ * @brief Represents an HTTP client connection. A pointer to the server is 
+ * kept, along with the IP the client is coming from, the socket used 
+ * to communicate with it and the sockaddr_in info.
+ */
 typedef struct http_client_s {
     http_server_s *server;
     char *ip;
@@ -36,9 +49,38 @@ typedef struct http_client_s {
     socklen_t addr_len;
 } http_client_s;
 
+/**
+ * @brief Accept a client connection.
+ * @param server The HTTP server.
+ * @return A pointer to an http_client_s structure representing the client
+ * connection. This must be freed with http_client_close(). Returns NULL on 
+ * error.
+ */
 extern http_client_s *http_accept(http_server_s *server);
+
+/**
+ * @brief Closes a client connection and cleans up allocated resources.
+ * @param client The client connection to close.
+ * @return nothing
+ */
 extern void http_client_close(http_client_s *client);
+
+/**
+ * @brief Closes the HTTP server connection.
+ * @param server The HTTP server to close.
+ * @return nothing
+ */
 extern void http_close(http_server_s *server);
+
+/**
+ * @brief Initializes the HTTP server. The server is setup to listen on the
+ * given IP address and port and will use the passed log.
+ * @param log The log handle to write to.
+ * @param server_ip IP for the server to listen on.
+ * @param port Port for the server to listen on.
+ * @return Pointer to the http_server_s representing the HTTP connection, or
+ * NULL on error. 
+ */
 extern http_server_s *http_init(log_s *log, char *server_ip, int port);
 
-#endif // HTTP_H
+#endif // http_H
--- response.h
+++ response.h
@@ -12,6 +12,9 @@
 
 #include <stdlib.h>
 
+/**
+ * @brief HTTP response codes: 200, 501, etc.
+ */
 typedef enum http_response_code_e {
     HTTP_RESPONSE_200 = 0,
     HTTP_RESPONSE_400 = 1,
@@ -20,12 +23,30 @@ typedef enum http_response_code_e {
     HTTP_RESPONSE_501 = 4,
 } http_response_code_e;
 
+/**
+ * @brief Represents an HTTP response.
+ */
 typedef struct http_response_s {
     struct request_s *request;
 } http_response_s;
 
+/**
+ * @brief Table of response code strings mapped to http_response_code_e codes.
+ */
 extern const char const *response_code_str[];
 
+/**
+ * @brief Generates an HTTP response header appropriate for the given 
+ * parameters.
+ * @param code The response code for the response.
+ * @param content_length Length of the content part.
+ * @param mime Mime type of the content part.
+ * @param additional_headers List of headers to send with the response.
+ * @param header_len Contains the size of the returned header.
+ * @return Returns a character string containing the header to send to the 
+ * client. The string is allocated dynamically and freeing it is the
+ * responsibility of the caller.
+ */
 extern char *http_response_header(http_response_code_e code, size_t content_length, const char *mime, const char *additional_headers, size_t *header_len);
 
 #endif // RESPONSE_H
